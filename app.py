744a9d3fa72e4e0cbdd4fd0119dd89c405b8135e
Sharvinee Ragunatha Rao <6550157+sharvinee@users.noreply.github.com>
2026-05-07 16:12:54 +0000

    updated port and requirements

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, jsonify, render_template, request
 import csv
+import os
+
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
@@ -92,4 +94,5 @@ def spin_options():
     })
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5001)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(debug=True, host="0.0.0.0", port=port)
